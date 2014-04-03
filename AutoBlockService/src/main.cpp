32c290dcf4dc71abad66bff6b506b8f06589cfa6
Ragaeeb Haq <7464039+ragaeeb@users.noreply.github.com>
2014-04-02 23:40:33 -0400

    GCR v2.5.1.0: Issue 70:	Allow turning on verbose logging.

--- AutoBlockService/src/main.cpp
+++ AutoBlockService/src/main.cpp
@@ -1,31 +1,17 @@
 #include "precompiled.h"
 
 #include "service.hpp"
+#include "Logger.h"
 
 using namespace bb;
 using namespace autoblock;
-
-#if !defined(QT_NO_DEBUG)
-namespace {
-
-FILE* f = NULL;
-
-void redirectedMessageOutput(QtMsgType type, const char *msg)
-{
-	Q_UNUSED(type);
-	fprintf(f, "%s\n", msg);
-}
-
-}
-#endif
 
 Q_DECL_EXPORT int main(int argc, char **argv)
 {
 	Application app(argc, argv);
 
-#if !defined(QT_NO_DEBUG)
-    f = fopen( QString( QDir::currentPath()+"/logs/service.log").toUtf8().constData(), "w");
-    qInstallMsgHandler(redirectedMessageOutput);
+#ifdef BETA
+    registerLogging("service.log");
 #endif
 
 	new Service(&app);

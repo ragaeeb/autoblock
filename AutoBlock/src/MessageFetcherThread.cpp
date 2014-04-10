cc27ccf537845bddda49e0dbdd50c06cca7cb55b
Ragaeeb Haq <geniewiz@gmail.com>
2014-04-09 22:10:18 -0400

    Issue 77:	App sometimes shows blank addresses being blocked when invoked via Share Framework through BlackBerry Hub.

--- AutoBlock/src/MessageFetcherThread.cpp
+++ AutoBlock/src/MessageFetcherThread.cpp
@@ -1,7 +1,6 @@
 #include "precompiled.h"
 
 #include "MessageFetcherThread.h"
-#include "MessageManager.h"
 #include "MessageImporter.h"
 #include "Logger.h"
 
@@ -19,13 +18,13 @@ MessageFetcherThread::MessageFetcherThread(QStringList const& tokens, QObject* p
 void MessageFetcherThread::run()
 {
     qint64 accountId = m_tokens[2].toLongLong();
-    qint64 messageId = m_tokens[4].toLongLong();
+    qint64 messageId = m_tokens[3].toLongLong();
 
     LOGGER("Tokens" << m_tokens);
     LOGGER("Message Tokens" << accountId << messageId);
 
-    MessageManager m(accountId);
-    Message message = m.getMessage(messageId);
+    MessageService m;
+    Message message = m.message(accountId, messageId);
 
     QVariantMap result = MessageImporter::transform(message);
     result["accountId"] = accountId;
